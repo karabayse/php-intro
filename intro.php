02a73a8a11f2e64389ef3ec60eceb9aafcbc9947
Kara Bayse <karabayse@gmail.com>
2018-01-05 15:18:55 -0600

    added array and for each

--- intro.php
+++ intro.php
@@ -1,4 +1,14 @@
-
 <?php
 echo "Hello, World!";
+?>
+
+<?php
+      $words = array("the ", "quick ", "brown ", "fox ",
+      "jumped ", "over ", "the ", "lazy ", "dog ");
+      shuffle($words);
+      foreach ($words as $word) {
+          echo $word;
+      };
+
+      unset($word);
 ?>
